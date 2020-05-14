a8433c7427265b2fa43421a62d4da717e15eb83a
Junyangz <junyangz.iie@gmail.com>
2020-05-14 12:24:51 +0800

    Init commit

---
+++ iterm2-recv-zmodem.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Author: Matt Mastracci (matthew@mastracci.com) | modified to iTerm2 only by Junyangz (junyangz.iie@gmail.com)
+# AppleScript from http://stackoverflow.com/questions/4309087/cancel-button-on-osascript-in-a-bash-script
+# licensed under cc-wiki with attribution required 
+# Remainder of script public domain
+
+# osascript -e 'tell application "iTerm2" to version' > /dev/null 2>&1 && NAME=iTerm2 || NAME=iTerm
+# if [[ $NAME = "iTerm" ]]; then
+# 	FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+# else
+# 	FILE=`osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+# fi
+
+osascript -e 'tell application "iTerm2" to version' > /dev/null 2>&1 && \
+FILE=`osascript \
+-e 'tell application "iTerm2" to activate' \
+-e 'tell application "iTerm2" to set thefile to choose folder with prompt "Choose a folder to place received files in"' \
+-e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+
+if [[ $FILE = "" ]]; then
+	echo Cancelled.
+	# Send ZModem cancel
+	echo -e \\x18\\x18\\x18\\x18\\x18
+	sleep 1
+	echo
+	echo \# Cancelled transfer
+else
+	cd "$FILE"
+	/usr/local/bin/rz -E -e -b
+	sleep 1
+	echo
+	echo
+	echo \# Sent \-\> $FILE
+fi
